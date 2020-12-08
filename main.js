84f04ddbb74b389b752fbf33e656a91c5639eaaa
Ethan Moss <Ethanmoss2001@gmail.com>
2020-12-08 14:35:19 +0000

    setting greeting dependant on time of day

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const showTime = () => {
 
   hours = hours % 12 || 12;
 
-  time.innerHTML = `${hours}:${minutes}:${addZero(seconds)}`;
+  time.innerHTML = `${hours}:${minutes}:${addZero(seconds)} ${amPm}`;
 
   setTimeout(showTime, 1000);
 };
@@ -21,4 +21,18 @@ const addZero = (n) => {
   return (parseInt(n, 10) < 10 ? "0" : "") + n;
 };
 
+const setGreeting = () => {
+  let today = new Date();
+  let hours = today.getHours();
+
+  if (hours < 12) {
+    greeting.textContent = "Good Morning";
+  } else if (hours >= 12 && hours < 18) {
+    greeting.textContent = "Good Afternoon";
+  } else {
+    greeting.textContent = "Good Evening";
+  }
+};
+
 showTime();
+setGreeting();
