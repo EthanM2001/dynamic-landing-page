a5f884ece621a61e385f80807920a2a636a03e20
Ethan Moss <Ethanmoss2001@gmail.com>
2020-12-08 15:05:32 +0000

    added background images dependant on time of day

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const showTime = () => {
 
   hours = hours % 12 || 12;
 
-  time.innerHTML = `${hours}:${minutes}:${addZero(seconds)} ${amPm}`;
+  time.innerHTML = `${hours}:${addZero(minutes)}:${addZero(seconds)} ${amPm}`;
 
   setTimeout(showTime, 1000);
 };
@@ -21,18 +21,25 @@ const addZero = (n) => {
   return (parseInt(n, 10) < 10 ? "0" : "") + n;
 };
 
-const setGreeting = () => {
+const setImageAndGreeting = () => {
   let today = new Date();
   let hours = today.getHours();
 
   if (hours < 12) {
+    document.body.style.backgroundImage = "url(../images/morning.jpg)";
+    document.body.style.backgroundSize = "cover";
     greeting.textContent = "Good Morning";
   } else if (hours >= 12 && hours < 18) {
     greeting.textContent = "Good Afternoon";
+    document.body.style.backgroundImage = "url(../images/afternoon.jpg)";
+    document.body.style.backgroundSize = "cover";
   } else {
     greeting.textContent = "Good Evening";
+    document.body.style.backgroundImage = "url(../images/night.jpg)";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.color = "white";
   }
 };
 
 showTime();
-setGreeting();
+setImageAndGreeting();
