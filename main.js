54364fba6c18258919d94ceac56ffead59d99761
Ethan Moss <Ethanmoss2001@gmail.com>
2020-12-08 14:18:31 +0000

    Getting current time on screen

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-const time = document.getElementById("time");
-const greeting = document.getElementById("greeting");
+let time = document.getElementById("time");
+let greeting = document.getElementById("greeting");
+
+const showTime = () => {
+  let today = new Date();
+
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  let seconds = today.getSeconds();
+
+  let amPm = hours < 12 ? "AM" : "PM";
+
+  hours = hours % 12 || 12;
+
+  time.innerHTML = `${hours}:${minutes}:${seconds}`;
+
+  setTimeout(showTime, 1000);
+};
+
+showTime();
